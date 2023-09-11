814b13a66a701d5bd10752a304c2daeb09fab73a
louiskhenghao <louiskhenghao@gmail.com>
2023-09-11 16:17:26 +0800

    refactor(backend): consolidate constant & config & remove platform header interceptor

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,26 +1,16 @@
-import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { PROJECT_NAME } from '@apps/config/constant';
-import { NestFactory, Reflector } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { LoggingInterceptor } from 'nestjs-dev-utilities';
 import { ConfigEnvironmentType as ENV } from '@stack/server';
-import { RequestHeaderPlatformInterceptor } from '@apps/interceptors/request-headers-platform.interceptor';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get<ConfigService<ENV>>(ConfigService);
 
-  const reflector = app.get(Reflector);
-
-  app.useGlobalInterceptors(
-    new LoggingInterceptor(),
-    new RequestHeaderPlatformInterceptor(
-      reflector,
-      app.get<JwtService>(JwtService)
-    )
-  );
+  app.useGlobalInterceptors(new LoggingInterceptor());
 
   app.useGlobalPipes(
     new ValidationPipe({
